e42ebc087423133b47c406067ee3f6b4b9c4d012
Corentin Martens <corentin.martens@ulb.be>
2021-06-10 15:45:51 +0200

    Change license to GPLv3.

--- PyCEST/utils.py
+++ PyCEST/utils.py
@@ -1,3 +1,22 @@
+# This file is part of the PyCEST package.
+# Copyright (C) 2021  Corentin Martens
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# Contact: corentin.martens@ulb.be
+
+
 def exchange_rate(k_0, k_a, k_b, ph, pk_w=14.0):
 
     """Computes the proton exchange rate from the spontaneous, acid, and base catalysis constants for a given pH value.
